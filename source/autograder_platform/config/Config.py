2040a775908581c12067ed205e477d0097189209
Gregory Bell <trihard.studios@outlook.com>
2025-02-03 16:17:42 -0700

    revert change of default

--- source/autograder_platform/config/Config.py
+++ source/autograder_platform/config/Config.py
@@ -174,7 +174,7 @@ class AutograderConfigurationSchema(BaseSchema[AutograderConfiguration]):
                     "autograder_version": And(str, Regex(r"\d+\.\d+\.\d+")),
                     "test_directory": And(str, os.path.exists),
                     "enforce_submission_limit": bool,
-                    Optional("submission_limit", default=1): And(int, lambda x: x >= 1),
+                    Optional("submission_limit", default=1000): And(int, lambda x: x >= 1),
                     Optional("take_highest", default=True): bool,
                     Optional("allow_extra_credit", default=False): bool,
                     "perfect_score": And(int, lambda x: x >= 1),
