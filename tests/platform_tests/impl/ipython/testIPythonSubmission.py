c0be31b9cacb8de370ddd1414c29ecde68291852
Gregory Bell <trihard.studios@outlook.com>
2025-05-18 13:34:05 -0600

    write builder for ipython submissions

--- tests/platform_tests/impl/ipython/testIPythonSubmission.py
+++ tests/platform_tests/impl/ipython/testIPythonSubmission.py
@@ -1,9 +1,11 @@
 import os.path
 import shutil
 import unittest
+from io import StringIO
+from unittest.mock import patch
 
 from autograder_platform.StudentSubmission.common import ValidationError
-from autograder_platform.StudentSubmissionImpl.ipython.IPythonSubmission import IPythonSubmission
+from autograder_platform.StudentSubmissionImpl.IPython.IPythonSubmission import IPythonSubmission
 from .NotebookBuilder import NotebookBuilder
 
 
@@ -11,7 +13,7 @@ class TestIPythonSubmission(unittest.TestCase):
     TEST_FILE_DIRECTORY: str = "./sandbox"
     VALID_TESTABLE_CELL: str = \
         """
-from autograder_platform.StudentSubmissionImpl.ipython.metadata import TestableCell
+from autograder_platform.StudentSubmissionImpl.IPython.metadata import TestableCell
 TestableCell(id="testable_cell", deps=[])
 print("VALID_TESTABLE_CELL")
     """
@@ -44,6 +46,29 @@ print("VALID_CELL")
 
         submission = IPythonSubmission()\
             .setSubmissionRoot(self.TEST_FILE_DIRECTORY)\
+            .load()
+
+        discoveredFiles = submission.getDiscoveredFiles()
+
+        self.assertIn(filename, discoveredFiles)
+
+        cells = submission.getCells()
+
+        self.assertEqual(2, len(cells))
+
+    def testDiscoverSingleFileNested(self):
+        fullpath = os.path.join(self.TEST_FILE_DIRECTORY, "nested")
+        os.mkdir(fullpath)
+        filename = NotebookBuilder("notebook.ipynb", fullpath) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell(self.VALID_TESTABLE_CELL) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell(self.VALID_CELL) \
+            .addMarkdownCell("# Markdown Cell") \
+            .toFile()
+
+        submission = IPythonSubmission() \
+            .setSubmissionRoot(self.TEST_FILE_DIRECTORY) \
             .load()
 
         discoveredFiles = submission.getDiscoveredFiles()
@@ -106,12 +131,13 @@ print("VALID_CELL")
 
         self.assertIn("at least one cell to be testable", msg.lower())
 
-    def testTestableCellWithDependency(self):
+    @patch('sys.stdout', new_callable=StringIO)
+    def testTestableCellWithDependency(self, capturedStdout):
         expected = 100
         NotebookBuilder("notebook.ipynb", self.TEST_FILE_DIRECTORY) \
             .addMarkdownCell("# Markdown Cell") \
             .addCodeCell(f"""
-from autograder_platform.StudentSubmissionImpl.ipython.metadata import Cell, TestableCell
+from autograder_platform.StudentSubmissionImpl.IPython.metadata import Cell, TestableCell
 Cell(id="imports")
 value = {expected}
             """) \
@@ -125,19 +151,25 @@ print(value)
 
         submission = IPythonSubmission()\
             .setSubmissionRoot(self.TEST_FILE_DIRECTORY)\
-            .load()
+            .load()\
+            .build()\
+            .validate()
 
         cells = submission.getCells()
 
         self.assertIn("testable", cells.keys())
         self.assertEqual("imports", cells["testable"].metadata.deps[0].id)
 
+        exec(submission.getExecutableSubmission())
+
+        self.assertEqual(f"{expected}\n", capturedStdout.getvalue())
+
     def testTestableCellMissingDependency(self):
         expected = 100
         NotebookBuilder("notebook.ipynb", self.TEST_FILE_DIRECTORY) \
             .addMarkdownCell("# Markdown Cell") \
             .addCodeCell(f"""
-from autograder_platform.StudentSubmissionImpl.ipython.metadata import Cell, TestableCell
+from autograder_platform.StudentSubmissionImpl.IPython.metadata import Cell, TestableCell
 Cell(id="imports")
 value = {expected}
             """) \
@@ -157,6 +189,88 @@ print(value)
         msg = str(ex.exception)
 
         self.assertIn("missing dependency 'dne' for cell 'testable'", msg.lower())
+
+    def testOnlyRunnableCellsCanBeActivated(self):
+        NotebookBuilder("notebook.ipynb", self.TEST_FILE_DIRECTORY) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell(self.VALID_TESTABLE_CELL) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell(self.VALID_CELL) \
+            .addMarkdownCell("# Markdown Cell") \
+            .toFile()
+
+        submission = IPythonSubmission() \
+            .setSubmissionRoot(self.TEST_FILE_DIRECTORY) \
+            .load()\
+            .build()\
+            .validate()
+
+        with self.assertRaises(RuntimeError) as ex:
+            submission.setActiveCell("cell")
+
+        msg = str(ex.exception)
+
+        self.assertIn("invalid active cell", msg.lower())
+
+    def testActiveCellMustBeSetWhenManyUnset(self):
+        NotebookBuilder("notebook.ipynb", self.TEST_FILE_DIRECTORY) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell(f"""
+from autograder_platform.StudentSubmissionImpl.IPython.metadata import Cell, TestableCell
+Cell(id="imports")
+            """) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell("""
+TestableCell(id="testable", deps=[(0, "imports")])
+            """) \
+            .addCodeCell(self.VALID_TESTABLE_CELL) \
+            .addMarkdownCell("# Markdown Cell") \
+            .toFile()
+
+        submission = IPythonSubmission() \
+            .setSubmissionRoot(self.TEST_FILE_DIRECTORY) \
+            .load() \
+            .build() \
+            .validate()
+
+        with self.assertRaises(RuntimeError) as ex:
+            submission.getExecutableSubmission()
+
+        msg = str(ex.exception)
+
+        self.assertIn("no active cell has been defined", msg.lower())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def testActiveCellMustBeSetWhenManySet(self, capturedStdout):
+        expected = "imported!"
+        NotebookBuilder("notebook.ipynb", self.TEST_FILE_DIRECTORY) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell(f"""
+from autograder_platform.StudentSubmissionImpl.IPython.metadata import Cell, TestableCell
+TestableCell(id="imports", deps=[])
+value = '{expected}'
+            """) \
+            .addMarkdownCell("# Markdown Cell") \
+            .addCodeCell("""
+TestableCell(id="testable", deps=[(0, "imports")])
+print(value)
+            """) \
+            .addCodeCell(self.VALID_TESTABLE_CELL) \
+            .addMarkdownCell("# Markdown Cell") \
+            .toFile()
+
+        submission = IPythonSubmission() \
+            .setSubmissionRoot(self.TEST_FILE_DIRECTORY) \
+            .load() \
+            .build() \
+            .validate()
+
+        submission.setActiveCell("testable")
+
+        exec(submission.getExecutableSubmission())
+
+        self.assertEqual(f"{expected}\n", capturedStdout.getvalue())
+
 
     def testGenerateHTMLFailsWhenDisabled(self):
         NotebookBuilder("notebook2.ipynb", self.TEST_FILE_DIRECTORY) \
