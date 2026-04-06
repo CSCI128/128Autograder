7c10277c7d48493ce87995ae338526cb7eaad81d
Tristen <ublius.developer@gmail.com>
2026-04-06 16:44:09 -0600

    Remove red text in diff log for better clarity when highlighting

--- source/autograder_platform/TestingFramework/Assertions.py
+++ source/autograder_platform/TestingFramework/Assertions.py
@@ -15,8 +15,7 @@ class Assertions(unittest.TestCase):
     RED_BG: str = u"\u001b[41m"
     RED_COLOR: str = u"\u001b[31m"
     GREEN_BG: str = u"\u001b[42m"
-    YELLOW_COLOR: str = u"\u001b[33m"
-    RESET: str = u"\u001b[0m"
+    RESET_COLOR: str = u"\u001b[0m"
 
     def __init__(self, testResults):
         super().__init__(testResults)
@@ -76,10 +75,8 @@ class Assertions(unittest.TestCase):
     def _highlightStringDifferences(expected: str, actual: str) -> str:
         """Return diff log strings with differences highlighted."""
         RED_BG = Assertions.RED_BG  
-        RED_COLOR = Assertions.RED_COLOR
         GREEN_BG = Assertions.GREEN_BG
-        YELLOW_COLOR = Assertions.YELLOW_COLOR
-        RESET = Assertions.RESET
+        RESET_COLOR = Assertions.RESET_COLOR
 
         matcher = difflib.SequenceMatcher(None, expected, actual)
         
@@ -88,16 +85,16 @@ class Assertions(unittest.TestCase):
         for tag, i1, i2, j1, j2 in matcher.get_opcodes():
             if tag == 'equal':
                 for ch in expected[i1:i2]:
-                    diff_log.append(f"{GREEN_BG}{RED_COLOR}{ch}{RESET}")
+                    diff_log.append(f"{RESET_COLOR}{GREEN_BG}{ch}{RESET_COLOR}")
             elif tag == 'replace':
                 for ch in actual[j1:j2]:
-                    diff_log.append(f"{RED_BG}{YELLOW_COLOR}{ch}{RESET}")
+                    diff_log.append(f"{RESET_COLOR}{RED_BG}{ch}{RESET_COLOR}")
             elif tag == 'delete':
                 for ch in expected[i1:i2]:
-                    diff_log.append(f"{RED_BG}{YELLOW_COLOR}{ch}{RESET}")
+                    diff_log.append(f"{RESET_COLOR}{RED_BG}{ch}{RESET_COLOR}")
             elif tag == 'insert':
                 for ch in actual[j1:j2]:
-                    diff_log.append(f"{RED_BG}{YELLOW_COLOR}{ch}{RESET}")
+                    diff_log.append(f"{RESET_COLOR}{RED_BG}{ch}{RESET_COLOR}")
         
         return ''.join(diff_log)
 
